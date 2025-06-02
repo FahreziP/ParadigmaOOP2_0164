a7d8d527c5fa5f2d77c3db1eb8fabbc8b9da2e06
FahreziP <fahrezipurwadianitra@gmail.com>
2025-06-02 15:07:36 +0700

    mengambil codingan dari noPewarisan

---
+++ noOverriding.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class baseClass{
+    public:
+        virtual void perkenalan()final {
+            cout << "Halo saya function dari base class";
+        }
+};
+
+class derivedClass : public baseClass{
+    public:
+        void perkenalan(){
+            cout << "Halo saya fungsi dari derived class";
+        }
+};
+
+int main (){
+    
+}
