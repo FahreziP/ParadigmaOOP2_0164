66c8634ab7e3f409d2e90a6cafb29f0801bbe5ae
FahreziP <fahrezipurwadianitra@gmail.com>
2025-06-02 14:23:18 +0700

    menambahkan class kamu

--- polimorphy.cpp
+++ polimorphy.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+class kamu{
+    public:
+    // virtual void pesan() = 0;
+    virtual void pesan(){
+        cout << "Pesan dari kamu" << endl;
+
+    }
+};
+
 
 int main (){
 
